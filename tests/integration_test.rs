6132829612c2a4b003f30c38ba1fb2f84d149987
Johan Govers <johan.govers@technolution.nl>
2023-08-05 15:24:36 +0200

    update testcase

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,7 +3,9 @@ use osm_bpf_to_svg::*;
 
 #[test]
 fn test_10_by_10_km() -> Result<()> {
-    let select_box = SelectBox::new(170_000, 180_000, 1_000, 1_000);
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    let select_box = SelectBox::new(170_000, 440_000, 1_000, 1_000);
     osm_bpf_to_svg::doit(
         select_box,
         "gelderland-latest.osm.pbf".to_string(),
